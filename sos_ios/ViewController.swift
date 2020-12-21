fe179e74c0e6b3183001d9566bcb5043b78b85d7
Haris Dautovic <haris.dautovic.dev@gmail.com>
2020-12-21 18:58:24 +0100

    Initial Commit

---
+++ sos_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sos_ios
+//
+//  Created by Haris Dautovic Development on 21/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sos_iosTests/sos_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  sos_iosTests.swift
+//  sos_iosTests
+//
+//  Created by Haris Dautovic Development on 21/12/2020.
+//
+
+import XCTest
+@testable import sos_ios
+
+class sos_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
